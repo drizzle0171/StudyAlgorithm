443fe331b4a4534a6f60fff179734e63d19b4f98
drizzle0171 <yongsandew@gmail.com>
2022-06-21 21:43:06 +0900

    11727

--- BOJ/11727: 2xn 타일링 2.py
+++ BOJ/11727: 2xn 타일링 2.py
@@ -1,13 +1,12 @@
 import sys
 
 n = int(sys.stdin.readline())
-dp = [0] * (n+2)
+dp = [0]*(n+2)
+
 dp[1] = 1
-dp[2] = 2
+dp[2] = 3
 
 for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+    dp[i] = dp[i-2] * 2 + dp[i-1]
 
-for i in dp:
-    print(i)
 print(dp[n]%10007)
