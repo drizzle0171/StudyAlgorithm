8a8318414f711f5b7138b9dc8889a2c20550119e
drizzle0171 <yongsandew@gmail.com>
2022-06-22 18:26:01 +0900

    9095

---
+++ BOJ/9095: 1,2,3 더하기.py
@@ -0,0 +1,18 @@
+import sys
+
+T = int(sys.stdin.readline())
+cnt = 0
+
+while T != cnt: 
+    n = int(sys.stdin.readline())
+    dp = [0] * (n+3)
+
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
+
+    for i in range(4, n+1):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
+
+    print(dp[n])
+    cnt +=1
