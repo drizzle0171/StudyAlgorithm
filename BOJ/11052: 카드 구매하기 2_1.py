a8eb7dd345a80ea0b4a69e134e6a7159acc610ba
drizzle0171 <yongsandew@gmail.com>
2022-06-24 17:15:13 +0900

    16194

---
+++ BOJ/11052: 카드 구매하기 2_1.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = list(map(int, input().split()))
+lst = [0] + lst
+
+dp = [999999999 for i in range(1001)]
+dp[0] = 0
+
+for i in range(n + 1):
+    for j in range(i + 1):
+        dp[i] = min(dp[i], lst[j] + dp[i - j])
+
+print(dp[n])
