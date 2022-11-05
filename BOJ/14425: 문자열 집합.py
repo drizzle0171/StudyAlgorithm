d0b4dadf982b301343222c0b428dd043188e1b36
drizzle0171 <yongsandew@gmail.com>
2022-11-05 23:48:16 +0900

    14425

---
+++ BOJ/14425: 문자열 집합.py
@@ -0,0 +1,13 @@
+N, M = map(int, input().split())
+
+S = set()
+for _ in range(N):
+    S.add(input())
+
+cnt = 0
+for _ in range(M):
+    t = input()
+    if t in S:
+        cnt += 1
+
+print(cnt)
