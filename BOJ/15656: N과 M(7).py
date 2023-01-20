49cf7e5682eb6499d12365cc36325ded71c34122
drizzle0171 <yongsandew@gmail.com>
2023-01-20 23:42:13 +0900

    15656

---
+++ BOJ/15656: N과 M(7).py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+num = sorted(list(map(int, input().split())))
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(" ".join(map(str, s)))
+        return
+
+    for i in range(n):
+        s.append(num[i])
+        dfs()
+        s.pop()
+
+dfs()
