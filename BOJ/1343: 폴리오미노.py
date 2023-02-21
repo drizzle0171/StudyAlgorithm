63023951b39b7a4c9e78337cae07d522fbc79a21
drizzle0171 <yongsandew@gmail.com>
2023-02-21 12:58:54 +0900

    1343

--- BOJ/1343: 폴리오미노.py
+++ BOJ/1343: 폴리오미노.py
@@ -5,25 +5,43 @@ s = input().rstrip()
 result = ''
 cnt = 0
 
-for i in range(len(s)):
-    print(cnt, s[i], result)
+for i in range(len(s)-1):
     if s[i] == 'X':
         cnt += 1
-        if cnt == 4:
-            result += 'AAAA'
-            cnt = 0
-        elif cnt == 2:
+    if cnt == 2:
+        if s[i+1] != 'X':
             result += 'BB'
             cnt = 0
+    if cnt == 4:
+        result += 'AAAA'
+        cnt = 0
     if s[i] == '.':
-        if cnt == 4:
-            result += 'AAAA'
-            cnt = 0
-        elif cnt == 2:
-            result += 'BB'
-            cnt = 0
+        result += '.'
+        cnt = 0
 
-if len(result) == 0:
+    
+if s[-1] == 'X':
+    cnt += 1
+
+if cnt == 2:
+    result += 'BB'
+
+if cnt == 4:
+    result += 'AAAA'
+
+if len(result) != len(s):
     print(-1)
 else:
     print(result)
+    
+
+board = input()
+
+board = board.replace("XXXX", "AAAA")
+board = board.replace("XX", "BB")
+
+if 'X' in board:
+    print(-1)
+    
+else:
+    print(board)
