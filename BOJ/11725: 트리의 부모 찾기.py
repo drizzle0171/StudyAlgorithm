44ee8cf384a7f13fd2303c7667ff2d37c77376f8
drizzle0171 <yongsandew@gmail.com>
2023-03-21 14:52:36 +0900

    11725

---
+++ BOJ/11725: 트리의 부모 찾기.py
@@ -0,0 +1,50 @@
+# import sys
+# input = sys.stdin.readline
+
+# n = int(input())
+# graph = [[] for i in range(n+1)]
+# for _ in range(n-1):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# def dfs(k, m):
+#     visited[k] = 1
+#     for i in graph[k]:
+#         if i == m:
+#             print(k)
+#             return
+#         else:
+#             if visited[i] == 0:
+#                 visited[i] = 1  
+#                 dfs(i, m)
+
+# for j in range(2, n+1):
+#     visited = [0] * (n+1)
+#     dfs(1, j)
+
+import sys
+sys.setrecursionlimit(10**6)
+n = int(sys.stdin.readline())
+
+graph = [[] for i in range(n+1)]
+
+for i in range(n-1):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0]*(n+1)
+
+arr = []
+
+def dfs(s):
+    for i in graph[s]:
+        if visited[i] == 0:
+            visited[i] = s
+            dfs(i)
+
+dfs(1)
+
+for x in range(2, n+1):
+    print(visited[x])
